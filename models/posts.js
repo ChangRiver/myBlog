d3ca3968c1015826ee7ffb246c13d5dee9c21b3a
changriver <changriver@suboat.com>
2016-11-29 16:51:39 +0800

    feat: 添加留言

--- models/posts.js
+++ models/posts.js
@@ -3,6 +3,7 @@
  */
 var Post = require('../lib/mongo').Post;
 var marked = require('marked');
+var CommentModel = require('./comments');
 
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml', {
@@ -20,6 +21,29 @@ Post.plugin('contentToHtml', {
     }
 });
 
+// 给 post 添加留言数 commentsCount
+Post.plugin('addCommentsCount', {
+    afterFind: function(posts) {
+        return Promise.all(posts.map(function (post) {
+            return CommentModel.getCommentsCount(post._id)
+                .then(function(commentsCount) {
+                   post.commentsCount = commentsCount;
+                    return post;
+                });
+        }));
+    },
+    afterFindOne: function(post) {
+        if(post) {
+            return CommentModel.getCommentsCount(post._id).then(function (count) {
+               post.commentsCount = count;
+                return post;
+            });
+        }
+
+        return post;
+    }
+})
+
 module.exports = {
     create: function create(post) {
         return Post.create(post).exec();
@@ -29,7 +53,7 @@ module.exports = {
         return Post
             .findOne({ _id: postId })
             .populate({ path: 'author', model: 'User' })
-            .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -43,8 +67,9 @@ module.exports = {
             .find(query)
             .populate({ path: 'author', model: 'User' })
             .sort({ _id: -1 })
-            addCreatedAt()
-                .exec();
+            .addCommentsCount()
+            .contentToHtml()
+            .exec();
     },
     // 通过文章 id 给 pv 加 1
     incPv: function incPv(postId) {
